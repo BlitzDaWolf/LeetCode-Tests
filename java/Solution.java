239c573a38e5efaf38b200a445372a7c1e1c3342
BlitzDaWolf <botjes2@hotmail.com>
2023-08-25 00:47:35 +0200

    Done plusOne Challange

--- java/Solution.java
+++ java/Solution.java
@@ -56,4 +56,22 @@ public class Solution {
         String[] res = s.trim().split(" ");
         return res[res.length-1].length();
     }
+
+    public int[] plusOne(int[] digits) {
+        int res = 1;
+        for (int i = digits.length-1; i > -1;i--){
+            int result = digits[i] + res;
+            res = (result - (result % 10)) / 10;
+            digits[i]=result%10;
+            if (res == 0) break;
+        }
+        if(res != 0){
+            int[] tmp = new int[digits.length + 1];
+            tmp[0] = res;
+            for (int i =0; i < digits.length;i++) tmp[i+1] = digits[i];
+            digits = tmp;
+            // Add new field
+        }
+        return digits;
+    }
 }
--- java/main.java
+++ java/main.java
@@ -1,6 +1,9 @@
 public class main {
     public static void main(String[] args) {
         Solution sln = new Solution();
-        sln.lengthOfLastWord("   fly me   to   the moon  ");
+        int[] a = sln.plusOne(new int[]{1,2,3});
+        int[] b = sln.plusOne(new int[]{4,3,2,1});        
+        int[] c = sln.plusOne(new int[]{9});
+        System.out.println("Completed all the tests");
     }
 }
