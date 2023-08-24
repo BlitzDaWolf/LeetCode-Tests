b691c49940188f8302bddcc20dad3dbcc18177c5
BlitzDaWolf <botjes2@hotmail.com>
2023-08-25 00:30:10 +0200

    Added first commit

---
+++ java/Solution.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs, 0, strs.length, (String a, String b) ->Integer.compare(a.length(), b.length()));
+        String best = "";
+
+        for (int i = 1; i <= strs[0].length(); i++)
+        {
+            String sub = strs[0].substring(0, i);
+            Boolean contains = true;
+            for (int j = 0; j < strs.length; j++){
+                if(!strs[j].startsWith(sub)){
+                    contains = false;
+                    break;
+                }
+            }
+            
+            if(contains == false) break;
+            best = sub;
+        }
+
+        return best;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null&&list2==null) return null;
+        List<ListNode> nodes= new ArrayList<ListNode>() {};
+        {
+            ListNode current = list1;
+            while (current != null)
+            {
+                nodes.add(current);
+                current = current.next;
+            }
+        }
+        {
+            ListNode current = list2;
+            while (current != null)
+            {
+                nodes.add(current);
+                current = current.next;
+            }
+        }
+        nodes.sort((ListNode a, ListNode b)-> Integer.compare(a.val, b.val));
+        for (int i = 0; i < nodes.size() - 1; i++)
+        {
+            nodes.get(i).next = nodes.get(i+1);// .next = nodes[i + 1];
+        }
+        return nodes.get(0);
+    }
+
+    public int lengthOfLastWord(String s) {
+        String[] res = s.trim().split(" ");
+        return res[res.length-1].length();
+    }
+}
---
+++ java/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        Solution sln = new Solution();
+        sln.lengthOfLastWord("   fly me   to   the moon  ");
+    }
+}
