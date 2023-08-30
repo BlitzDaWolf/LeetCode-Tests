7727e6e900955de520abb2b7f975ae1949207d6d
BlitzDaWolf <botjes2@hotmail.com>
2023-08-30 23:48:13 +0200

    Finished `getConcatenation`

---
+++ java/Solutions/getConcatenationClass.java
@@ -0,0 +1,14 @@
+package Solutions;
+
+public class getConcatenationClass {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length*2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i];
+            res[i + nums.length] = nums[i];
+        }
+
+        return res;
+    }
+}
